ede82d06b600eef83e276be027d4c4cb9e645ad0
Akmal <akmaljon.gafforov@gmail.com>
2022-10-31 09:03:11 +0500

    - Removed unused env params

--- config/first.php
+++ config/first.php
@@ -1,12 +1,11 @@
 <?php
 
 return [
-    'merchant_id' => env('FIRST_MERCHANT_ID', 0),
-    'merchant_key' => env('FIRST_MERCHANT_KEY', ''),
-    'payment_limit' => env('FIRST_PAYMENT_LIMIT', 0),
     'except' => ['sign'],
     'hash_algo' => 'sha256',
     'separator' => ':',
+    'merchant_key' => env('FIRST_MERCHANT_KEY', ''),
+    'payment_limit' => env('FIRST_PAYMENT_LIMIT', 0),
     'signature_name' => 'sign',
     'signature_source' => 'body'
 ];
--- config/second.php
+++ config/second.php
@@ -1,12 +1,11 @@
 <?php
 
 return [
-    'merchant_id' => env('SECOND_APP_ID', 0),
-    'merchant_key' => env('SECOND_APP_KEY', ''),
-    'payment_limit' => env('SECOND_PAYMENT_LIMIT', 0),
     'except' => [],
     'hash_algo' => 'md5',
     'separator' => '.',
+    'merchant_key' => env('SECOND_APP_KEY', ''),
+    'payment_limit' => env('SECOND_PAYMENT_LIMIT', 0),
     'signature_name' => 'Authorization',
     'signature_source' => 'header'
 ];
